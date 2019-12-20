7a5c258dbf9698b912f9be5f6679ebbea54dcb8d
gururchandran <gururchandran1@gmail.com>
2019-12-21 00:57:29 +0530

    new

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 // const cors = require('cors')
 // const path = require('path');
-// const MongoConMongoConnect.connect(common_config.mongo_URL, function (err, client) {
+const MongoConnect = require('mongodb').MongoClient;
+// const body_parser = require('body-parser
+
+// MongoConnect.connect(common_config.mongo_URL, function (err, client) {
     
 //     if (err) console.log(err)
 //     else {
@@ -11,18 +14,18 @@ const app = express()
 //             console.log('Server is up and running on ' + port)
 //         })
 //     }
-// })nect = require('mongodb').MongoClient;
-// const body_parser = require('body-parser')
+// }))
 
 // const controller = require('./controller/controller.js')
 // const gallery = require('./controller/gallery.js')
 // const slider = require('./controller/slider.js')
-// const contact = require('./application-server/controller/contact.js')
+const contact = require('./application-server/controller/contact.js')
+
+const port = process.env.PORT || 3000
 
 
 
-
-// const common_config = require('./application-server/config/server_config.json')
+const common_config = require('./application-server/config/server_config.json')
 // var model =  require('./model.js')
 
 
@@ -39,35 +42,35 @@ const app = express()
 // app.use('/',controller)
 // app.use('/slider',slider)
 // app.use('/gallery',gallery)
-// app.use('/contact',contact)
+app.use('/contact',contact)
 
 
-// MongoConnect.connect(common_config.mongo_URL, function (err, client) {
+MongoConnect.connect(common_config.mongo_URL, function (err, client) {
     
-//     if (err) console.log(err)
-//     else {
-//         app.locals.db = client.db('skoolerz');
-//         app.listen(port, '0.0.0.0', () => {
-//             console.log('Server is up and running on ' + port)
-//         })
-//     }
-// })
+    if (err) console.log(err)
+    else {
+        app.locals.db = client.db('skoolerz');
+        app.listen(port, '0.0.0.0', () => {
+            console.log('Server is up and running on ' + port)
+        })
+    }
+})
 
 // common_config.node_port
 
 
-const http = require('http');
-const port = process.env.PORT || 3000
+// const http = require('http');
+// const port = process.env.PORT || 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end('<h1>Hello World</h1>');
+// });
 
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
-});
+// server.listen(port,() => {
+//   console.log(`Server running at port `+port);
+// });
 
 
 // common_config.node_port
