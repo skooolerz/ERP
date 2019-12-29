3aa17d65c762ec4352231496ab066d790e1963ee
HariKrish <harikrish1296@gmail.com>
2019-12-29 15:53:47 +0530

    multer

--- application-server/models/upload.js
+++ application-server/models/upload.js
@@ -1,22 +1,22 @@
-const multer = require('multer')
+// const multer = require('multer')
 
-let slider_storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './public/slider')
-    },
-    filename: function (req, file, cb) {
-        let extArray = file.mimetype.split("/");
-        let extension = extArray[extArray.length - 1];
-        cb(null, file.fieldname + '-' +Date.now()+ '.' +extension)
-    }
-})
-let slider_upload = multer({
-    storage: slider_storage
-}).single('image')
+// let slider_storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, './public/slider')
+//     },
+//     filename: function (req, file, cb) {
+//         let extArray = file.mimetype.split("/");
+//         let extension = extArray[extArray.length - 1];
+//         cb(null, file.fieldname + '-' +Date.now()+ '.' +extension)
+//     }
+// })
+// let slider_upload = multer({
+//     storage: slider_storage
+// }).single('image')
 
 exports.slider_upload_api_image = function (req, res, next) {
     slider_upload(req,res,(err)=>{
-        if(err instanceof multer.MulterError){
+        if(err){
             console.log("Error while uploading",err)
             res.json({status:199})
         }else if(err){
